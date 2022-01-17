0e971431071850ef836391b9cb85c76cd25cf4b8
Andy Chukavin <megadestroyer007@gmail.com>
2022-01-17 14:11:50 +0400

    Initial Commit

---
+++ SolanaTest/SolanaTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SolanaTestApp.swift
+//  SolanaTest
+//
+//  Created by Andrey Chukavin on 17.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct SolanaTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
